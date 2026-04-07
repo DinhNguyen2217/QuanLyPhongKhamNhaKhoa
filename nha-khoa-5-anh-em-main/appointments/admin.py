898fc97191e054a200874c533d30b31d4af223f2
buinhatlam <buinhatlam1906@gmail.com>
2026-04-07 23:01:18 +0700

    Delete nha-khoa-5-anh-em-main directory

--- nha-khoa-5-anh-em-main/appointments/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Appointment # Hoặc tên Model bạn đã đặt
-
-@admin.register(Appointment)
-class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('fullname', 'phone', 'created_at') # Hiện các cột này ra danh sách
-    search_fields = ('fullname', 'phone') # Cho phép tìm kiếm theo tên hoặc số ĐT
--- nha-khoa-5-anh-em-main/backend/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Nhớ để là backend.settings vì thư mục của bạn tên là backend
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-application = get_wsgi_application()
